2a13fb573891798aa5d1a1beb3b1aa2c07c60a6e
Bianca Pelcastre Varela <bianka@iMac-de-Bianca.local>
2023-02-27 06:38:48 -0600

    Clases y objetos de Mokepon

--- src/app.js
+++ src/app.js
@@ -28,6 +28,18 @@ let ataqueEnemigo
 let vidasJugador = 3
 let vidasEnemigo = 3 
 
+class Mokepon{
+    constructor(nombre,foto,vida){
+        this.nombre = nombre
+        this.foto = foto
+        this.vida = vida
+    }
+}
+
+let hipodoge = new Mokepon('Hipodoge','./imagenes/hipodoge_attack.png','5')
+
+console.log(hipodoge);
+
 function iniciarJuego() { 
     sectionSeleccionarAtaque.style.display = 'none' // ocultar secciones de html que no ocupemos hasta despues // se oculta el boton de reiniciar
     selecionarMascota.addEventListener('click', seleccionarMascotaJugador);
